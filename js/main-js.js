a73b5cc19770ce0b99bc1e32b394722084d7d611
Ruzlan Karim <ruzlankarim2@gmail.com>
2018-09-13 12:31:27 +0600

    all content made editable
    all content have been made editable
    waiting on backend api to interect with ...

--- js/main-js.js
+++ js/main-js.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+
+// function editQues(){
+//   var edit
+// }
+
+ function ApplyEdit(id){
+  alert(id);
+  var question = document.getElementById("editQues").innerHTML;
+
+  var optionA = document.getElementById("editOpA").innerHTML;
+  var optionB = document.getElementById("editOpB").innerHTML;
+  var optionC= document.getElementById("editOpC").innerHTML;
+  var optionD= document.getElementById("editOpD").innerHTML;
+
+  var meta = document.getElementById("editMeta").innerHTML;
+
+  var comment =
+  document.getElementById("editComment").innerHTML;
+
+  // write a code to comunicate with backend
+
+
+
+}
+function ApplyDelete(id){
+  alert(id);
+
+  // write a code to talk with back end 
+}
